c4f0e71af980d3af1425406d1253a12da62ce0c3
Caitlin Greer <clento1121@gmail.com>
2021-03-09 11:52:34 -0500

    added dotenv

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const MOVIEDEX = require('./moviedex.json')
+
+console.log(process.env.API_TOKEN)
 
 const app = express()
 
 app.use(morgan('dev'))
 
 app.get('/movie', function handleGetMovies(req, res) {
-    let response = MOVIEDEX.movie
-    console.log(response)
+    let response = MOVIEDEX
     res.json(response)
 })
 
